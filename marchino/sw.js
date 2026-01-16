59f4ce3617d3124457d94a4a5049146b145213e1
forbicina <stompet@proton.me>
2026-01-16 16:34:50 +0100

    adding notification

--- marchino/sw.js
+++ marchino/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'marchino-v2.5';
+const CACHE_NAME = 'marchino-v3';
 const ASSETS = [
     '/marchino/',
     '/marchino/index.html',
@@ -6,8 +6,8 @@ const ASSETS = [
     '/css/ciessesse.css',
     '/newtab/js/citazioni.js',
     '/assets/ico.ico',
-    '/marchino/icon-192.png?v=2.5',
-    '/marchino/icon-512.png?v=2.5',
+    '/marchino/icon-192.png?v=3',
+    '/marchino/icon-512.png?v=3',
     ];
 
 self.addEventListener('install', e => {
